5b5e3c792e2a4065390e14cca3dd4f587cbe6e8e
Luckianchikov Max <feltmax@gmail.com>
2018-07-19 20:43:35 +0600

    Add antdesign ui lib

--- src/Pages/Homepage/index.js
+++ src/Pages/Homepage/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { DatePicker } from 'antd';
 
 function Homepage() {
-  return <div>Homepage screen</div>;
+  return (
+    <div>
+      Homepage screen
+      <div>
+        <DatePicker />
+      </div>
+    </div>
+  );
 }
 
 export default Homepage;
